a9912d158b14b7ea3d901bb5841ca47c3851514b
Humaid Khan <humaidhoumam2000@gmail.com>
2026-03-28 14:22:39 +0530

    Initial project setup with springboot

---
+++ demo/src/test/java/com/plant/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plant.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
